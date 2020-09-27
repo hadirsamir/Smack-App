6646cfacdae116f7c266b42d135c2faf928d8c7e
HadirAbdElReheem <hadirhamedmahmoud>
2020-09-27 20:00:05 +0200

    add viewControllers

---
+++ team-devslopes-app-smack-fe494f15fe51/Smack/Views/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Mac on 9/27/20.
+//  Copyright © 2020 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
