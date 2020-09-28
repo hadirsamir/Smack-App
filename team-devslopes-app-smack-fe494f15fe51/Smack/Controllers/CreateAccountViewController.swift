194cd59a4b5814f83205554984f2feb7993ab3e8
HadirAbdElReheem <hadirhamedmahmoud>
2020-09-28 23:50:34 +0200

    add create account VC

---
+++ team-devslopes-app-smack-fe494f15fe51/Smack/Controllers/CreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccountViewController.swift
+//  Smack
+//
+//  Created by Mac on 9/28/20.
+//  Copyright © 2020 Jonny B. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
